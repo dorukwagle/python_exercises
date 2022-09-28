2fc070a0fa25cb67865aa6da46b6ce2b7cc6e439
Doruk Wagle <dorukwagle@gmail.com>
2022-09-28 22:35:50 +0545

    prime number checker added

--- check_prime.py
+++ check_prime.py
@@ -1,2 +1,14 @@
+from re import T
+
+
 def is_prime(number):
-    
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is prime number")
+else:
+    print(f"{num} is not a prime number")
